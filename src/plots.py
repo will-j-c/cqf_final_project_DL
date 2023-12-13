0f700c96fb04e1c428a2eadd951e3c2d689542ae
Will <willjcGA@outlook.com>
2023-12-13 21:35:00 +0800

    feat: added new data source, added EDA on days, months and hours

--- src/plots.py
+++ src/plots.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 plt.style.use('ggplot')
 
 
-def plot_price_vol(df):
+def plot_price_vol(df, close_col='Close', vol_col='Volume'):
     # Credit to this article for the basic flow for this method https://medium.com/analytics-vidhya/visualizing-historical-stock-price-and-volume-from-scratch-46029b2c5ef9
     fig, axes = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]})
     fig.tight_layout(pad=3)
-    close = df['Close']
-    vol = df['Volume']
+    close = df[close_col]
+    vol = df[vol_col]
     # Set the axis
     plot_price = axes[0]
     plot_vol = axes[1]
@@ -29,4 +30,11 @@ def plot_price_vol(df):
     for plot in [plot_price, plot_vol]:
         plot.spines['top'].set_visible(False)
         plot.spines['left'].set_visible(False)
+    plt.show()
+
+def plot_scatter(x, y, x_ticks_entries, title):
+    plt.scatter(x, y)
+    plt.xticks(x_ticks_entries)
+    # Plot trend line
+    plt.title(title)
     plt.show()
