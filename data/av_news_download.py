1c2e44edaeecd9a0db677d079ab9f12c2782e1db
Will <willjcGA@outlook.com>
2023-12-18 23:45:22 +0800

    chore: working on data sentiment data, end of day commit

--- data/av_news_download.py
+++ data/av_news_download.py
@@ -3,29 +3,50 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 import json
+import datetime
+import time
 
 # Load environment variables
 load_dotenv('.env')
 
 api_key = os.environ['AV_API']
 base_url = f'https://www.alphavantage.co/query'
+start_date = datetime.datetime(2021, 12, 13, 1)
+end_date = datetime.datetime(2023, 12, 12, 23)
 params = {
     'function': 'NEWS_SENTIMENT',
     'tickers': 'CRYPTO:ETH',
     'topics': 'blockchain',
     'limit': 1000,
+    'time_from': start_date.strftime('%Y%m%dT%H%M'),
     'apikey': api_key
 }
-# r = requests.get(base_url, params=params)
-# print(api_key)
-# print(r.json())
-# feed = r.json()['feed']
-with open('data.json') as f:
-    feed = json.load(f)['feed']
+delta = datetime.timedelta(seconds=1)
+i = 1
 arr = []
-
-for item in feed:
-    arr.append((item['time_published'], item['overall_sentiment_score']))
+while datetime.datetime.strptime(params['time_from'], '%Y%m%dT%H%M') < end_date:
+    try:
+        # Make request
+        r = requests.get(base_url, params=params)
+        print(r.url)
+        feed = r.json()['feed']
+        # Extract required values
+        for item in feed:
+            # Append the values to the arr
+            arr.append((item['time_published'], item['overall_sentiment_score']))
+        # Set date to last record received plus 1 second
+        print(feed[-1]['time_published'])
+        new_date = datetime.datetime.strptime(
+            feed[-1]['time_published'], '%Y%m%dT%H%M%S')
+        params['time_from'] = (new_date + delta).strftime('%Y%m%dT%H%M')
+        print(params['time_from'])
+        time.sleep(5)
+        if i == 25:
+            break
+    except Exception as e:
+        print(e)
+        print('Hits this ')
+        break
 
 df = pd.DataFrame(arr, columns=['datetime', 'sentiment'])
 df['datetime'] = pd.to_datetime(df['datetime'])
