46defd2df219a5196738dbfe80e048567f0d9d74
Will <willjcGA@outlook.com>
2023-12-20 22:15:42 +0800

    feat: added script to download fear and greed, reorganised files

---
+++ data/greed_fear_index.py
@@ -0,0 +1,10 @@
+import requests
+import pandas as pd
+
+r = requests.get('https://api.alternative.me/fng/?limit=0')
+data = r.json()['data']
+df = pd.DataFrame(data)
+df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s').dt.strftime('%Y-%m-%d %H:%M:%S')
+df.index = df['timestamp']
+# df.drop(['timestamp', 'time_until_update'], axis=0, inplace=True)
+df.to_csv('data/raw/crypto_greed_fear_index.csv')
