03654d04ec196194eb7efd2fdb03cbb8be0096fc
Will <willjcGA@outlook.com>
2023-12-30 23:42:44 +0800

    feat: drafted baseline model and got it working in test

--- src/feature_select_reduce.py
+++ src/feature_select_reduce.py
@@ -6,6 +6,10 @@ import pandas as pd
 import numpy as np
 
 # Preprocessing
+from sklearn.model_selection import train_test_split
+
+# Metrics
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 # Feature selection
 from boruta import BorutaPy
@@ -22,13 +26,26 @@ X_train = pd.read_csv('data/train/scaled_X_train.csv', parse_dates=True, index_c
 y_train = pd.read_csv('data/train/y_train.csv', parse_dates=True, index_col='unix')
 X_test = pd.read_csv('data/test/scaled_X_test.csv', parse_dates=True, index_col='unix')
 y_test = pd.read_csv('data/test/y_test.csv', parse_dates=True, index_col='unix')
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, shuffle=False)
 
 # Calculate the weights for the imbalanced classes
 
+
 # Reshape the data into the correct format
+seqlen = 24
+featurelen = X_train.shape[-1]
+train_tensors = tf.keras.utils.timeseries_dataset_from_array(X_train, y_train, seqlen)
+val_tensors = tf.keras.utils.timeseries_dataset_from_array(X_val, y_val, seqlen)
+test_tensors = tf.keras.utils.timeseries_dataset_from_array(X_test, y_test, seqlen)
 
 # Baseline model
-inputs = tf.keras.Input()
+inputs = tf.keras.Input(shape=(seqlen, featurelen))
 x = tf.keras.layers.LSTM(16)(inputs)
 outputs = tf.keras.layers.Dense(1)(x)
 model = tf.keras.Model(inputs, outputs)
+
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=5)]
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+history
