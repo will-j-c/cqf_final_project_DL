3468ac154fd01b40d98e046ae234b70a2a9ded60
Will <willjcGA@outlook.com>
2023-12-31 18:28:25 +0800

    feat: feature selection script sort of works

--- src/feature_select_reduce.py
+++ src/feature_select_reduce.py
@@ -4,9 +4,12 @@ from helpers import *
 # Import base
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import time
 
 # Preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 
 # Metrics
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
@@ -15,24 +18,36 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from boruta import BorutaPy
 from sklearn.ensemble import RandomForestClassifier
 
+# Dimentionality reduction
+from umap import UMAP
+
 # tensorflow
 import tensorflow as tf
 
+# Warnings
+import warnings
+warnings.filterwarnings("ignore")
+
 # Set seeds for reproducibility
 set_seeds()
+
+# Clear any backend
+tf.keras.backend.clear_session()
 
 # Reload the saved scaled data
 X_train = pd.read_csv('data/train/scaled_X_train.csv', parse_dates=True, index_col='unix')
 y_train = pd.read_csv('data/train/y_train.csv', parse_dates=True, index_col='unix')
 X_test = pd.read_csv('data/test/scaled_X_test.csv', parse_dates=True, index_col='unix')
 y_test = pd.read_csv('data/test/y_test.csv', parse_dates=True, index_col='unix')
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, shuffle=False)
+X_val = pd.read_csv('data/val/scaled_X_val.csv', parse_dates=True, index_col='unix')
+y_val = pd.read_csv('data/val/y_val.csv', parse_dates=True, index_col='unix')
 
 # Calculate the weights for the imbalanced classes
-
+y = pd.concat([y_train, y_val, y_test])
+weights = cwts(y.values.flatten())
 
 # Reshape the data into the correct format
-seqlen = 24
+seqlen = 1
 featurelen = X_train.shape[-1]
 train_tensors = tf.keras.utils.timeseries_dataset_from_array(X_train, y_train, seqlen)
 val_tensors = tf.keras.utils.timeseries_dataset_from_array(X_val, y_val, seqlen)
@@ -40,12 +55,70 @@ test_tensors = tf.keras.utils.timeseries_dataset_from_array(X_test, y_test, seql
 
 # Baseline model
 inputs = tf.keras.Input(shape=(seqlen, featurelen))
-x = tf.keras.layers.LSTM(16)(inputs)
-outputs = tf.keras.layers.Dense(1)(x)
+x = tf.keras.layers.LSTM(36)(inputs)
+outputs = tf.keras.layers.Dense(units=1, activation='sigmoid')(x)
 model = tf.keras.Model(inputs, outputs)
 
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(patience=5)]
+# Compile baseline classifier model
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+              metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
-history
+# Define the various feature selection methods
+rf = RandomForestClassifier(n_jobs=-1, class_weight=weights)
+vif = VIFTransform()
+boruta = BorutaPy(rf, n_estimators='auto', verbose=2)
+umap = UMAP(n_neighbors=5)
+
+# Define data pipelines
+pipelines = [
+    'none',
+    Pipeline([('vif', vif)], verbose=True),
+    Pipeline([('vif', vif), ('boruta', boruta)], verbose=True),
+    Pipeline([('boruta', boruta)], verbose=True),
+    Pipeline([('boruta', boruta), ('umap', umap)], verbose=True)
+]
+
+for pipe in pipelines:
+    # Time the run
+    start = time.time()
+    # Create file path for run
+    filepath = './tensorboard/feature_selection/run'
+    time_str = datetime.now().strftime('%m-%d-%Y-%H:%M:%S')
+    if pipe == 'none':
+        filepath += '_all_'
+    else:
+        for key in pipe.named_steps.keys():
+            filepath += f'_{key}_'
+    filepath += time_str
+    
+    # Callbacks
+    callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=5),
+    tf.keras.callbacks.TensorBoard(log_dir=filepath, histogram_freq=1)]
+    
+    if pipe == 'none':
+        model.fit(x=train_tensors, epochs=100, validation_data=val_tensors,
+                    class_weight=weights, callbacks=callbacks)
+        
+        end = time.time()
+        duration = '{0:.5f}'.format(end - start)
+        print(f'Duration of pipeline: {duration} seconds')
+        # Continue the loop
+        continue
+    
+    # Create the output of the pipeline
+    X_train_pipe = pipe.fit_transform(X_train, y_train.values.ravel())
+    X_val_pipe = pipe.transform(X_val)
+
+    # Convert the output to tensors
+    seqlen = 1
+    featurelen = X_train.shape[-1]
+    train_tensors = tf.keras.utils.timeseries_dataset_from_array(X_train_pipe, y_train, seqlen)
+    val_tensors = tf.keras.utils.timeseries_dataset_from_array(X_val_pipe, y_val, seqlen)
+    # test_tensors = tf.keras.utils.timeseries_dataset_from_array(X_test_pipe, y_test, seqlen)
+    # Fit the models
+    model.fit(x=train_tensors, epochs=100, validation_data=val_tensors,
+                    class_weight=weights, callbacks=callbacks)
+    end = time.time()
+    duration = '{0:.5f}'.format(end - start)
+    print(f'Duration of pipeline: {duration} seconds')
--- src/helpers.py
+++ src/helpers.py
@@ -29,7 +29,7 @@ class VIFTransform(BaseEstimator, TransformerMixin):
         vif.reset_index(inplace=True, drop=True)
         return vif
     
-    def fit_transform(self, X):
+    def fit_transform(self, X, y=0):
         X = X.copy()
         print('Calculating VIF Factors')
         self.vif = self._calc_vif(X)
@@ -46,19 +46,29 @@ class VIFTransform(BaseEstimator, TransformerMixin):
         X =  X[self.vif_features]
         # Update bool for run
         self.fit_transform_run = True
-        return X
+        return X.values
         
     def transform(self, X):
         # Transform the data based on the 
         if self.fit_transform_run:
             X = X.copy()
             X =  X[self.vif_features]
-            return X
+            return X.values
         else:
             # If you haven't run fit_transform yet to the training data
             raise Exception('Please run fit_transform on training data')
         
+    def fit(self, X, y=0):
+        return self.fit_transform(X)
     
     def summary(self):
         vif = self.vif
         return vif
+
+# Calculate class weights. Returns dict of class weights
+# Credit to Kannan from Advanced ML I, CQF
+def cwts(y):
+    c0, c1 = np.bincount(y)
+    w0=(1/c0)*(len(y))/2 
+    w1=(1/c1)*(len(y))/2 
+    return {0: w0, 1: w1}
