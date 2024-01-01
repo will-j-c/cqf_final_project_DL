d7430bd85d0e3cb081faaed5be9e6f6bac209927
Will <willjcGA@outlook.com>
2024-01-01 22:31:36 +0800

    feat: tweaks to feature_seleciton

--- log.py
+++
@@ -1,7 +0,0 @@
-time_taken = 100
-run_name = 'string'
-
-lines = [f'Time taken: {time_taken}', f'Run name: {run_name}']
-
-with open('log.txt', 'w') as f:
-    f.writelines('\n'.join(lines))
--- src/config.py
+++
@@ -1,5 +0,0 @@
-from src.plots import *
-from src.helpers import *
-
-# Set the random seek
-set_seeds()
